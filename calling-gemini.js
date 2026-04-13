0eda4075a32501f128e67046aab1ab6f80dc50a9
SRCreator <85744292+onu032001@users.noreply.github.com>
2026-04-14 07:57:09 +0900

    Create calling-gemini.js

---
+++ calling-gemini.js
@@ -0,0 +1,20 @@
+// Use for safe purposes
+const API_KEY = '';
+async function askGemini(prompt, systemInstructions) {
+    const payload = {
+        contents: [{parts: [{text: prompt}]}],
+        systemInstruction: [{parts: [{text: systemInstructions}]}]
+    };
+    
+    const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key=${API_KEY}`;
+    const options = {
+        method: 'POST',
+        contentType: 'application/json',
+        body: JSON.stringify(payload)
+    };
+
+    const response = await fetch(url, options);
+    const data = await response.json();
+    const content = data['candidates'][0]['content']['parts'][0]['text'];
+    return content;
+}
